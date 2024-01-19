d9b7bdf0a85361d97b63d9f6a756680675070456
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-19 17:35:09 +0300

    shadcn installed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,8 +3,12 @@ import SideNav from "@/components/SideNav"
 export default function DashboardLayout({ children }:{ children: React.ReactNode;}) {
   return (
     <div className="flex h-screen relative flex-col md:flex-row md:overflow-hidden">
-        <div>
+        <div className="md:w-20 lg:w-64 flex-none md:border-r w-full">
             <SideNav />
+        </div>
+
+        <div className="flex-grow mt-12 md:mt-0 w-full md:overflow-y-auto sm:p-6 md:p-12 max-w-7xl mx-auto">
+            {children}
         </div>
 
     </div>
