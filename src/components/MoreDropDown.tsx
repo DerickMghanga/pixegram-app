5b695f8bd7c61f61a8724b3b945516eea9f986c6
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-23 22:16:54 +0300

    dark & light mode added

--- src/components/MoreDropDown.tsx
+++ src/components/MoreDropDown.tsx
@@ -9,17 +9,19 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "./ui/button"
-import { Activity, Bookmark, LogOut, Menu, Moon, Settings } from "lucide-react"
+import { Activity, Bookmark, ChevronLeft, LogOut, Menu, Moon, Settings, Sun } from "lucide-react"
 import { useEffect, useRef, useState } from "react"
 import { cn } from "@/lib/utils"
+import { useTheme } from "next-themes"
+import { Label } from "./ui/label"
+import { Switch } from "./ui/switch"
 
 export default function MoreDropDown() {
 
     const [showModeToggle, setShowModalToggle] = useState(false);
-
     const [open, setOpen] = useState(false);
-
     const ref = useRef<HTMLDivElement>(null); //access and modify DOM elements, preserve values and manage state without triggering unnecessary re-renders.
+    const { theme, setTheme } = useTheme();
 
     useEffect(() => {
         //close the dropdown when the user clicks outside the menu element
@@ -58,40 +60,73 @@ export default function MoreDropDown() {
                 align="end"
                 alignOffset={-40}
             >
-            {
-                !showModeToggle && (
-                    <>
-                        <DropdownMenuItem className="menuItem">
-                            <Settings size={20} />
-                            <p>Settings</p>
-                        </DropdownMenuItem>
+                {
+                    !showModeToggle && (
+                        <>
+                            <DropdownMenuItem className="menuItem">
+                                <Settings size={20} />
+                                <p>Settings</p>
+                            </DropdownMenuItem>
 
-                        <DropdownMenuItem className="menuItem">
-                            <Activity size={20} />
-                            <p>Your activity</p>
-                        </DropdownMenuItem>
+                            <DropdownMenuItem className="menuItem">
+                                <Activity size={20} />
+                                <p>Your activity</p>
+                            </DropdownMenuItem>
 
-                        <DropdownMenuItem className="menuItem">
-                            <Bookmark size={20} />
-                            <p>Saved</p>
-                        </DropdownMenuItem>
+                            <DropdownMenuItem className="menuItem">
+                                <Bookmark size={20} />
+                                <p>Saved</p>
+                            </DropdownMenuItem>
 
-                        <DropdownMenuItem className="menuItem"
-                            onClick={() => setShowModalToggle(true)}
-                        >
-                            <Moon size={20} />
-                            <p>Switch appearence</p>
-                        </DropdownMenuItem>
+                            <DropdownMenuItem className="menuItem"
+                                onClick={() => setShowModalToggle(true)}
+                            >
+                                <Moon size={20} />
+                                <p >Switch appearence</p>
 
-                        <DropdownMenuItem className="menuItem">
-                            <LogOut size={20} />
-                            <p>Log out</p>
-                        </DropdownMenuItem>
-                    </>
-                )
-            }
+                            </DropdownMenuItem>
+
+                            <DropdownMenuItem className="menuItem">
+                                <LogOut size={20} />
+                                <p>Log out</p>
+                            </DropdownMenuItem>
+                        </>
+                    )
+                }
+
+                {
+                    showModeToggle && (
+                        <>
+                            <div className="flex items-center border-b border-gray-200 dark:border-neutral-700 py-3.5 px-2.5">
+                                <ChevronLeft size={18} 
+                                    onClick={()=>setShowModalToggle(false)}
+                                />
+                                <p className="font-bold ml-1">Switch appearence</p>
+
+                                {
+                                    theme === "dark" ? (
+                                        <Moon size={20} className="ml-auto" />
+                                    ) : (
+                                        <Sun size={20} className="ml-auto" />
+                                    )
+                                }
+                            </div>
+
+                            <Label htmlFor="dark-mode" className="menuItem">
+                                Dark Mode
+                                <DropdownMenuItem className="ml-auto !p-0">
+                                    <Switch
+                                        id="dark-mode"
+                                        className="ml-auto"
+                                        checked={theme === "dark"}
+                                        onCheckedChange={(checked)=>setTheme(checked ? "dark" : "light")}
+                                    />
+                                </DropdownMenuItem>
+                            </Label>
+                        </>
+                    )
+                }
             </DropdownMenuContent>
-
         </DropdownMenu>
     )
 }
