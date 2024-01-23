937200be4f024e17ab3668e5474a3f93295ccdb5
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-24 00:42:22 +0300

    Next auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,13 @@
-export const config = {
-    
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import prisma from "@/lib/prisma"
+
+export const config: NextAuthOptions = {
+    pages: {
+        signIn:'/login'
+    },
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        
+    ]
 }
