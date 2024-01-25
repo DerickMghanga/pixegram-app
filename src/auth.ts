8556fb74345b80448210d6bccaa668a3bc5f0c15
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-26 01:14:30 +0300

    schema prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,36 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import prisma from "@/lib/prisma"
+import GoogleProvider from "next-auth/providers/google";
 
-export const config: NextAuthOptions = {
+export const config:NextAuthOptions = {
     pages: {
-        signIn:'/login'
+        signIn: '/login'
     },
     adapter: PrismaAdapter(prisma),
     providers: [
-        GoogleProvider: {
-            client: "",
-            clientSecret: ""
+        GoogleProvider: ({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+          })
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        async session({session, token}) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.name = token.name;
+                session.user.email = token.email;
+                session.user.image = token.picture;
+                session.user.username = token.username;
+            }
+
+            return session;
+        },
+        async jwt({token, user}) {
+            const prismaUser = await prisma.
         }
-    ]
+    }
 }
