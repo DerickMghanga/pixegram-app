1e75e975de0f13ba5dc9ce089cc8a1b73fe57193
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-26 02:54:18 +0300

    auth ts config

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,11 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth";
-import prisma from "@/lib/prisma"
+//import prisma from "@/lib/prisma"
+import { PrismaClient } from "@prisma/client"
 import GoogleProvider from "next-auth/providers/google";
+
+
+const prisma = new PrismaClient();
 
 export const config:NextAuthOptions = {
     pages: {
@@ -38,6 +42,11 @@ export const config:NextAuthOptions = {
 
             if (!prismaUser) {
                 token.id = user.id;
+                return token;
+            }
+
+            if (!prismaUser.username) {
+                
             }
         }
     }
