0bada761dac5864f07daba093ccff6d6ae5120fa
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-31 18:54:50 +0300

    next auth login button

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
+import LoginForm from '@/components/LoginForm'
 import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div>
+        <LoginForm />
+    </div>
   )
 }
